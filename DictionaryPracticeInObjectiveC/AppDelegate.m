3539a0431d5b316e3ae17d68d96b77e62c62359b
EMG114 <egutierrez411@gmail.com>
2016-06-06 23:26:27 -0400

    pull rqst

--- DictionaryPracticeInObjectiveC/AppDelegate.m
+++ DictionaryPracticeInObjectiveC/AppDelegate.m
@@ -24,6 +24,71 @@
      
      */
     
+    
+    
+    
+    NSDictionary *favoriteBands = [NSDictionary dictionary];
+    NSMutableDictionary *mFavoriteBands = [NSMutableDictionary dictionaryWithDictionary: favoriteBands];
+    
+    [mFavoriteBands setObject: @"Thriller" forKey: @"Michael Jackson"];
+    
+    mFavoriteBands[@"Prince"]= @"Purple Rain";
+    
+    mFavoriteBands[@"FleetWood Mac"]= @"Rumors";
+    
+    NSLog (@"%@",mFavoriteBands);
+    
+    
+    
+    
+    [mFavoriteBands removeObjectForKey:@"Prince"];
+    
+    NSLog (@"%@", mFavoriteBands);
+    
+    
+    
+    NSArray *favoriteBandAllKeys = [mFavoriteBands allKeys];
+    
+    for (NSString *band in favoriteBandAllKeys) {
+        NSLog(@"Band/Artist: %@ with album: %@", band, [mFavoriteBands objectForKey:band]);
+        
+    }
+    
+    if ([favoriteBandAllKeys containsObject: @"Michael Jackson"]) {
+        NSLog (@ "It has Michael Jackson!!!");
+    }
+    
+    
+    NSDictionary *Rumors = @{ @1 : @"Second Hand News",@2: @"Dreams",@3 : @"Never Going Back Again"};
+    NSDictionary *Thriller = @{@1: @"Wanna Be Startin' Somethin'",@2: @"Baby Be Mine",@3: @"The Girl Is Mine"};
+    
+    
+    [mFavoriteBands setObject:Rumors forKey:@"Fleetwood Mac"];
+    [mFavoriteBands setObject:Thriller forKey: @"Michael Jackson"];
+    
+    NSLog (@"This is the dictionary: %@", mFavoriteBands);
+    
+    
+    for (NSString *song in [mFavoriteBands[@"Michael Jackson"] allKeys]) {
+        NSLog(@"Song: %@", [mFavoriteBands[@"Michael Jackson"] objectForKey:song]);
+        
+    }
+    
+    
+    
+    
+    
+    NSMutableDictionary *favoriteAlbums = [NSMutableDictionary new];
+    
+    
+    for (NSString *Band in [mFavoriteBands allKeys]) {
+        [favoriteAlbums addEntriesFromDictionary:mFavoriteBands ];
+    }
+    
+    NSLog(@"Favorite albums: %@", favoriteAlbums);
+    
+    
+    
     // Do not alter
     return YES;  //
 }   ///////////////
